47eac430b01b44362d3d01808ceaf0d699d338b1
Targa82 <101151467+Targa82@users.noreply.github.com>
2022-04-04 15:25:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const modeswitch = document.querySelector(".modeswitch");
+const btnSearch = document.querySelector(".btn-search");
+const formMain = document.querySelector(".form");
+const input = document.querySelector("input");
+const userName = document.querySelector(".user-name");
+
+let img = document.createElement("img");
+let userImg = document.querySelector(".user-img");
 
 modeswitch.addEventListener("click", function (e) {
   const target = document.documentElement.getAttribute("data-theme");
@@ -7,4 +14,19 @@ modeswitch.addEventListener("click", function (e) {
   } else {
     document.documentElement.setAttribute("data-theme", "light");
   }
+});
+
+btnSearch.addEventListener("click", function () {
+  const apiUrl = `https://api.github.com/users/${input.value}`;
+  async function getUrl() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+
+    img.src = data.avatar_url;
+    userImg.appendChild(img);
+    userImg.style.border = "none";
+    userName.textContent = data.login;
+  }
+  getUrl();
 });
